a47839f73fe17a7f1bb6c3509383ebb8be2d1d0d
fajariadi-id <fajariadi.id@gmail.com>
2022-08-05 17:31:02 +0700

    update: colorize

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,10 +3,23 @@ import moment from "moment";
 
 const Card = ({ item }) => {
   const dateFormat = moment(item.createdAt).format("dddd, DD MMMM YYYY");
+  const maxTitleLength = 18;
+
+  const handleLongTitle = () => {
+    if (item.title.length > maxTitleLength) {
+      return `${item.title.substr(0, maxTitleLength)}...`;
+    } else {
+      return item.title;
+    }
+  };
+
   return (
     <div className="card">
-      <h3>{item.title}</h3>
-      <span>{dateFormat}</span>
+      <div>
+        {/* <div className="d-flex align-items-start justify-content-between"></div> */}
+        <h3>{handleLongTitle()}</h3>
+        <span>{dateFormat}</span>
+      </div>
 
       <p>{item.body}</p>
     </div>
